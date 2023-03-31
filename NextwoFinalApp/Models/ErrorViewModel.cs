1d1791496df3649263ff650ebe9bc86152727362
user <user@DESKTOP-ALERMIH>
2023-03-31 21:20:09 +0300

    Add project files.

---
+++ NextwoFinalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NextwoFinalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
